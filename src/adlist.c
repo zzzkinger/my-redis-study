12357ec9e9d24e860f85cfd1c782ce7e9e5d272f
Jin Mengzhe <564885257@qq.com>
2014-06-27 15:05:50 +0800

    测试添加一行注释

--- src/adlist.c
+++ src/adlist.c
@@ -27,7 +27,10 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-
+/*
+**
+*	´úÂëÑ§Ï°×¢ÊÍ--jinmengzhe
+*/
 
 #include <stdlib.h>
 #include "adlist.h"
